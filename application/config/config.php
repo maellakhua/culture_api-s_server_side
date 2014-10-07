c8c1b49af67a80b2fefac8aeb7460692a1b40215
ellakuser <ellakuser@dit2pc23>
2014-10-07 19:11:18 +0300

    base url insertet in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://62.217.125.30/~ellakuser';
 
 /*
 |--------------------------------------------------------------------------
