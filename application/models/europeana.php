ec0bc1867bff376e59e568c4ba9f7a2f1965af98
ellakuser <ellakuser@dit2pc16>
2014-10-13 17:26:00 +0300

    removed results with no lat long

--- application/models/europeana.php
+++ application/models/europeana.php
@@ -14,31 +14,35 @@ class Europeana extends CI_Model {
             $items = $decoded_info->items;
             $finaljson = '{"results":';
             $final_array = array();
-            foreach($items as $item) {       
-                $final_array[] = array("name" => isset($item->title) ? $item->title : '',
-                                       "lat" => isset($item->edmPlaceLatitude) ? $item->edmPlaceLatitude : '', 
-                                       "long" => isset($item->edmPlaceLongitude) ? $item->edmPlaceLongitude : '',
-                                       "address" => isset($item->edmIsShownAt) ? $item->edmIsShownAt : '',
-                                       "category" => '',
-                                       "source" => "Europeana");
+            foreach($items as $item) { 
+                if (isset($item->edmPlaceLatitude) && isset($item->edmPlaceLongitude)) {
+                    $final_array[] = array("name" => isset($item->title) ? $item->title : '',
+                                           "lat" => isset($item->edmPlaceLatitude) ? $item->edmPlaceLatitude : '', 
+                                           "long" => isset($item->edmPlaceLongitude) ? $item->edmPlaceLongitude : '',
+                                           "address" => isset($item->edmIsShownAt) ? $item->edmIsShownAt : '',
+                                           "category" => '',
+                                           "source" => "Europeana");
+                }
             }
             $finaljson .= json_encode($final_array).'}';
             return $finaljson;
         } else {
             $rlat = round($lat);
             $rlong = round($long);
-            $info = file_get_contents('http://europeana.eu/api/v2/search.json?wskey=qaVVXQUY6&query=pl_wgs84_pos_lat:['.$rlat.'+TO+'.($rlat+1).']+AND+pl_wgs84_pos_long:['.$rlong.'+TO+'.($rlong+1).'].&profile=minimal');
+            $info = file_get_contents('http://europeana.eu/api/v2/search.json?wskey=qaVVXQUY6&query=pl_wgs84_pos_lat:['.$rlat.'+TO+'.($rlat+0.01).']+AND+pl_wgs84_pos_long:['.$rlong.'+TO+'.($rlong+0.01).'].&profile=minimal');
             $decoded_info = json_decode($info);
             $items = $decoded_info->items;
             $finaljson = '{"results":';
             $final_array = array();
-            foreach($items as $item) {       
-                $final_array[] = array("name" => isset($item->title) ? $item->title : '',
-                                       "lat" => isset($item->edmPlaceLatitude) ? $item->edmPlaceLatitude : '', 
-                                       "long" => isset($item->edmPlaceLongitude) ? $item->edmPlaceLongitude : '',
-                                       "address" => isset($item->edmIsShownAt) ? $item->edmIsShownAt : '',
-                                       "category" => '',
-                                       "source" => "Europeana");
+            foreach($items as $item) { 
+                if (isset($item->edmPlaceLatitude) && isset($item->edmPlaceLongitude)) {
+                    $final_array[] = array("name" => isset($item->title) ? $item->title : '',
+                                           "lat" => isset($item->edmPlaceLatitude) ? $item->edmPlaceLatitude : '', 
+                                           "long" => isset($item->edmPlaceLongitude) ? $item->edmPlaceLongitude : '',
+                                           "address" => isset($item->edmIsShownAt) ? $item->edmIsShownAt : '',
+                                           "category" => '',
+                                           "source" => "Europeana");
+                }
             }
             $finaljson .= json_encode($final_array).'}';
             return $finaljson;
